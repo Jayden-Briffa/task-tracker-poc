d901dd6f3a0a11a4902ad6a6b087d27aefb275c7
Jayden-Briffa <jaydenbriffa@outlook.com>
2026-01-16 14:43:39 +0000

    feature/implement add_task()

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
+from queue import PriorityQueue as Priority_Queue
 
+current_tasks = dict()
+priority_idx = Priority_Queue()
+newest_id = 0
+
+def add_task(description, priority):
+    new_task = {
+        "id": newest_id,
+        "description": description,
+        "priority": priority,
+        "status": "incomplete"
+    }
+
+    current_tasks[new_task["id"]] = new_task
+    priority_idx.put_nowait((new_task["priority"], new_task["id"]))
+
+    return current_tasks[new_task["id"]]
 
 
 user_choice = None
 while True:
     user_choice = input("choice: ")
+    
+    match user_choice:
 
-    try:
-        match user_choice:
+        case "1":
+            description = input("Enter a description for the new task: ")
+            priority = input("Enter a priority for the new task: ")
 
-            case "1":
-                description = input("Enter a description for the new task: ")
-                priority = input("Enter a priority for the new task: ")
+            new_task = add_task(description, int(priority))
+            newest_id += 1
+            print(new_task)
 
-                # Add task
-                #print(new_task)
+        case "2":
+            # Get task by priority
+            # print(next_task)
+            pass
 
-            case "2":
-                # Get task by priority
-                # print(next_task)
-                pass
+        case "3":
+            id = input("Enter the task's id: ")
 
-            case "3":
-                id = input("Enter the task's id: ")
+            # Get task by id
+            # print(found_task))
 
-                # Get task by id
-                # print(found_task))
+        case "4":
+            id = input("Enter the task's id: ")
 
-            case "4":
-                id = input("Enter the task's id: ")
+            # Mark task as complete with id
+            # print(task)
 
-                # Mark task as complete with id
-                # print(task)
+        case "quit":
+            print("Shutting down...")
+            break
 
-            case "quit":
-                print("Shutting down...")
-                break
-
-            case _:
-                print("You must choose 1, 2, 3, or 4")
-
-    except:
-        print("An error occured. Please try again")
+        case _:
+            print("You must choose 1, 2, 3, or 4")
 
 
 quit()
